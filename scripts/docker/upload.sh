7d7b01190152d7162e706238d91e9e5ea9f36128
Nikita Omelchenko <nikomel@Nikitas-MBP.attlocal.net>
2022-04-18 14:25:58 -0700

    ci: update docker scripts

--- scripts/docker/upload.sh
+++ scripts/docker/upload.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
-
-# Assumes that an image is built via `run_docker.sh`
 
 # Step 1: Create dockerpath
-DOCKER_REPO=nikomel
-DOCKER_IMAGE=fastapi-app
+DOCKER_REPO=792765111837.dkr.ecr.us-east-1.amazonaws.com
+DOCKER_IMAGE=fastapi
 DOCKER_TAG=latest
 DOCKER_PATH=$DOCKER_REPO/$DOCKER_IMAGE:$DOCKER_TAG
 
 # Step 2: Authenticate & tag
 echo "Docker ID and Image: $DOCKER_PATH"
-docker login
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $DOCKER_REPO
 docker tag $DOCKER_IMAGE:$DOCKER_TAG $DOCKER_PATH
 
 # Step 3: Push image to a docker repository
